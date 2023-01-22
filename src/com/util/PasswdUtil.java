499ecb24e3acae797b04637d549b851af1e72fe0
rahul <lahuer99@gmail.com>
2023-01-23 01:45:54 +0530

    mod Passwdutil

--- src/com/util/PasswdUtil.java
+++ src/com/util/PasswdUtil.java
@@ -1,96 +1,22 @@
 package com.util;
 
-import java.time.LocalDateTime;
-import java.util.regex.Matcher;
-
 import com.model.Passwd;
 
-public class PasswdUtil implements BasePasswdInterface{
-	private String passwdText;
-	public void calcAndSetPasswdStrength(Passwd passwd) {
-		passwdText=passwd.getPasswdText();
-		int[] passwdStrengthBar=new int[] {-1,-1,-1,-1,-1};
-		//length>8,upper and lowercase,numbers,special characters
-		int currStre=0;
-		
-//		passwd length>8
-		if(passwdText.length()>8) {
-			currStre++;
-			passwdStrengthBar[0]=1;
-		}
-	
-//		use regex for uppercase and lowercase
-		if(containsUpper()) {
-			System.out.println("1up");
-			passwdStrengthBar[1]=1;
-			currStre++;
-		}
-		if(containsLower()) {
-			System.out.println("1lw");
-			passwdStrengthBar[2]=1;
-			currStre++;
-		}
-		
-//		use regex for nos
-		if(containsNos()) {
-			System.out.println("1nos");
-			passwdStrengthBar[3]=1;
-			currStre++;
-		}
-		
-//		use regex for spec
-		if(containsSpecials()) {
-			System.out.println("1spec");
-			passwdStrengthBar[4]=1;
-			currStre++;
-		}
-		
-		passwd.setPasswdStrength(currStre);
-		passwd.setPasswdStrengthBar(passwdStrengthBar);
-//		should happen if new passwd != old passwd
-		//		passwd.setDateLastModified(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+public class PasswdUtil{	
+	public void displayStrengthBar(Passwd passwd) {
+		String barIcon="=";
+		int amount=passwd.getPasswdStrength();
+		String padding=new String(new char[5-amount]).replace("\0", " ");
+		String finalBar=new String(new char[amount]).replace("\0", barIcon);
+		System.out.println("["+finalBar+">"+padding+"]");
 	}
 	
-	@Override
-	public boolean containsNos() {
-		// TODO Auto-generated method stub
-		Matcher nosMatcher=nosPattern.matcher(passwdText);
-		if(nosMatcher.find()) {
-			System.out.println("nos");
-			return true;
-		}
-		return false;
+	public String showModBetterPasswd(Passwd passwd) {
+//		should return the modified better version of this passwd
+//		ex : noob => N00b,nOoB,nO0b,nOOB1
+		
+		return "";
 	}
-
-	@Override
-	public boolean containsSpecials() {
-		Matcher specialMatcher=specialPattern.matcher(passwdText);
-		if(specialMatcher.find()) {
-			System.out.println("spec");
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public boolean containsUpper() {
-		Matcher specialMatcher=upperPattern.matcher(passwdText);
-		if(specialMatcher.find()) {
-			System.out.println("up");
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public boolean containsLower() {
-		Matcher specialMatcher=lowerPattern.matcher(passwdText);
-		if(specialMatcher.find()) {
-			System.out.println("lw");
-			return true;
-		}
-		return false;
-	}
-
 	
+		
 }
