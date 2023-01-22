6c88c18a45d2c6c9cbbde659c989329fdbb77831
rahul <lahuer99@gmail.com>
2023-01-22 15:33:18 +0530

    restructured

--- src/com/main/Index.java
+++ src/com/main/Index.java
@@ -3,24 +3,28 @@ package com.main;
 import java.util.Scanner;
 
 import com.model.Passwd;
+import com.persist.PasswdPersist;
+import com.util.PasswdUtil;
 
 public class Index {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		String str="6asdjkh#gbf7";//sc.next();
+		String str="777aSdjkh#gbf7";//sc.next();
 		
 		System.out.println(str);
 		
 		Passwd pwd=new Passwd(str);
-		
+		PasswdUtil pwdUtil=new PasswdUtil();
 //		how to set strength on initialization of object itself??
 //		calling methods from constr is bad practice
-		pwd.calcAndSetPasswdStrength();
-				
+		pwdUtil.calcAndSetPasswdStrength(pwd);
+		System.out.println(pwd.getPasswdStrength());
+		System.out.println(pwd.getPasswdStrengthBar());
+//		currently have strength and strengthBar to show bar and green/red over text in UI
 		if(pwd.getPasswdStrength()>1) {//get strength
 			//suggest modified passwd for same
+			PasswdPersist.saveToFile(pwd.getPasswdText());
 			System.out.println("gu");
 		}else {
 //			accept passwd
--- src/com/model/Passwd.java
+++ src/com/model/Passwd.java
@@ -2,10 +2,13 @@ package com.model;
 
 import java.util.regex.Matcher;
 
-public class Passwd implements PasswdInterface{//check serialization -bytestream,ciphers??
+public class Passwd{//check serialization -bytestream,ciphers??
 	private String passwdText;
 	private int passwdStrength;
-	
+	private int dateAdded;
+	private int dateLastModified;
+	private int[] passwdStrengthBar=new int[]{-1,-1,-1,-1,-1};
+
 	public Passwd(String passwdText) {
 		super();
 		this.passwdText = passwdText;
@@ -23,58 +26,18 @@ public class Passwd implements PasswdInterface{//check serialization -bytestream
 	public void setPasswdStrength(int passwdStrength) {
 		this.passwdStrength = passwdStrength;
 	}
-	
-	public void calcAndSetPasswdStrength() {
-		//length>8,upper and lowercase,numbers,special characters
-		int currStre=-1;
-		
-		if(passwdText.length()>8) {
-			currStre++;
-		}
-	
-//		use regex for uppercase and lowercase
-		
-		
-//		use regex for nos
-		if(containsNos()) {
-			System.out.println("1nos");
-			currStre++;
-		}
-		
-//		use regex for spec
-		if(containsSpecials()) {
-			System.out.println("1spec");
-			currStre++;
-		}
-		
-		setPasswdStrength(currStre);
+	public int[] getPasswdStrengthBar() {
+		return passwdStrengthBar;
 	}
-	
+
+	public void setPasswdStrengthBar(int[] passwdStrengthBar) {
+		this.passwdStrengthBar = passwdStrengthBar;
+	}
+
 	public String showModBetterPasswd() {
 //		should return the modified better version of this passwd
 //		ex : noob => N00b,nOoB,nO0b,nOOB1
 		return passwdText;
-	}
-
-	@Override
-	public boolean containsNos() {
-		// TODO Auto-generated method stub
-		Matcher nosMatcher=nosPattern.matcher(passwdText);
-		if(nosMatcher.find()) {
-			System.out.println("nos");
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public boolean containsSpecials() {
-		Matcher specialMatcher=specialPattern.matcher(passwdText);
-		if(specialMatcher.find()) {
-			System.out.println("spec");
-			return true;
-		}
-		return false;
 	}
 	
 }
